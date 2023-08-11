5781fece9bfa87ac9294fde670415fac3229acc1
KaushikiChopra <73760912+KaushikiChopra@users.noreply.github.com>
2023-08-11 13:25:16 +0530

    added cross-origin

--- expressRouter.js
+++ expressRouter.js
@@ -11,6 +11,17 @@ class Server {
       this.expressInstance = express();
       this.router = Router({ mergeParams: true });
       this.expressInstance.use(bodyParser.json());
+      this.expressInstance.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+    
+        next();
+    });
       this.expressInstance.use(bodyParser.urlencoded({ extended: true }))
 
       this.expressInstance.use('/api/v1', userRoutes)
